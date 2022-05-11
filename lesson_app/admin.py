b7045061b0affd92be4e2f3c72786eab3e1b5236
andrei <sherlat01@mail.ru>
2022-05-11 23:56:36 +0300

    display photos in admin panel

--- lesson_app/admin.py
+++ lesson_app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from lesson_app.models import Post, Comments, Profile, Like
+from django.contrib.auth.admin import UserAdmin as UserAdminBase
+from django.utils.safestring import mark_safe
+
+from lesson_app.models import Post, Comments, Profile, Like, User
 
 
 @admin.register(Post)
@@ -20,11 +23,25 @@ class PostAdmin(admin.ModelAdmin):
     list_display_links = ("post_id_id", "id")
 
 
-@admin.register(Profile)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ("user",)
-
-
 @admin.register(Like)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("id", "user_id", "post_id")
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    list_display = ("user", "get_image")
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f"<img src={obj.image.url} width='50' height='50' >")
+
+    get_image.short_description = "Аватарка"
+
+
+@admin.register(User)
+class UserAdmin(UserAdminBase):
+    inlines = (
+        ProfileInline,
+    )
--- lesson_app/models.py
+++ lesson_app/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.safestring import mark_safe
 
 
 class User(AbstractUser):
@@ -44,3 +45,5 @@ class Profile(models.Model):
 
     def get_absolute_url(self):
         return "/profile"
+
+
