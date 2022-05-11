71986a1806f3d1ae972cc795628a2067571eef8d
andrei <sherlat01@mail.ru>
2022-05-11 11:09:20 +0300

    delete photo on change

--- lesson_app/signals/register_user.py
+++ lesson_app/signals/register_user.py
@@ -1,6 +1,4 @@
-import os
-
-from django.db.models.signals import post_save, m2m_changed, pre_save
+from django.db.models.signals import post_save
 
 from django.dispatch import receiver
 
@@ -18,17 +16,3 @@ def post_save_user(**kwargs):
     if created:
         user_add = Profile(user=user)
         user_add.save()
-
-
-
-# @receiver(pre_save, sender=Profile)
-# def post_save_user(**kwargs):
-#     instance = kwargs["instance"]
-#     created = kwargs["created"]
-#     if not created:
-#         image = Profile.objects.get(pk=instance.id)
-#         print(image.image)
-#         # if image:
-#         #     path = f".\media\{image.image}"
-#         #     os.remove(path)
-#
