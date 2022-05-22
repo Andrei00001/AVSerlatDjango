ffbc73e91e7e210d1ff20cdd7d03525e231a116e
andrei <sherlat01@mail.ru>
2022-05-22 23:16:59 +0300

    update QuerySet post in TagsPostView

--- hashtag_app/api/views/tag_post.py
+++ hashtag_app/api/views/tag_post.py
@@ -6,10 +6,11 @@ from rest_framework.views import APIView
 
 from hashtag_app.api.serializers.tag_post import TagsPostSerializer
 from hashtag_app.models import Tags, PostTags
+from posts_app.models import Post
 
 
 class PaginationTags(LimitOffsetPagination):
-    default_limit =2
+    default_limit = 2
     offset_query_param = "offset"
     limit_query_param = 'limit'
     max_limit = 10000000
@@ -18,12 +19,8 @@ class PaginationTags(LimitOffsetPagination):
 class TagsPostView(APIView):
     def get(self, request, tag):
         tags = Tags.objects.get(tag=tag)
-        post = PostTags.objects.filter(tag=tags)
-        posts = list()
-
-        for i in post:
-            posts.append(i.post)
+        post = Post.objects.filter(tag_post__tag=tags)
         pg = PaginationTags()
-        page_roles = pg.paginate_queryset(queryset=posts, request=request, view=self)
-        serializer = TagsPostSerializer(instance = page_roles, many=True)
+        page_roles = pg.paginate_queryset(queryset=post, request=request, view=self)
+        serializer = TagsPostSerializer(instance=page_roles, many=True)
         return Response(serializer.data)
