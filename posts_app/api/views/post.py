68de88973701add9f2951a4a5670ad2b7402f4cb
andrei <sherlat01@mail.ru>
2022-06-29 13:48:10 +0300

    api count like in post

--- posts_app/api/views/post.py
+++ posts_app/api/views/post.py
@@ -2,13 +2,18 @@ from rest_framework import filters
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
 
-from posts_app.api.serializers.post import PostSerializer, PostImgSerializer
+from posts_app.api.serializers.post import PostSerializer, PostImgSerializer, LikePostSerializer
 from posts_app.models import Post, ImagePost
 
 
 class PostsView(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    action_serializer = {"retrieve": LikePostSerializer}
+    lookup_field = 'posts'
+
+    def get_serializer_class(self):
+        return self.action_serializer.get(self.action, self.serializer_class)
 
 
 class PostsImageView(GenericViewSet, ListModelMixin, CreateModelMixin):
