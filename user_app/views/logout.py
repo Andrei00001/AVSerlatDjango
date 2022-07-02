a98a6c25954005fccb3a062368eaf4cd6cfed4e8
andrei <sherlat01@mail.ru>
2022-07-02 23:08:49 +0300

    logaout post

--- user_app/views/logout.py
+++ user_app/views/logout.py
@@ -5,8 +5,8 @@ from django.views import View
 
 class Logout_user(View):
     def get(self, request):
-        return render(request, "user_app/logout.html")
+        return render(request, "user_app/login.html")
 
     def post(self, request):
         logout(request)
-        return redirect("login")
+        return self.get(request)
