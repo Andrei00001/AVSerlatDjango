c4353da6a6d23d21f8a142c3971a983428d6280e
andrei <sherlat01@mail.ru>
2022-07-04 16:02:44 +0300

    add sentry

--- Project/urls.py
+++ Project/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('', include('like_app.urls')),
     path('', include('people_app.urls')),
     path('', include('chat_app.urls')),
-    path('', include('errors.urls')),
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
